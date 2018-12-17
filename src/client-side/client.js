1d1162c6bcb16cdca4e79af92506a34c4aaa9154
amartinm7 <amartinm7@gmail.com>
2018-12-17 23:26:29 +0100

    refactoring the client code, adding babel support

--- src/client-side/client.js
+++ src/client-side/client.js
@@ -1,22 +1,32 @@
-import {ApolloClient} from 'apollo-client'
-import {HttpLink} from 'apollo-link-http'
-import {InMemoryCache} from "apollo-cache-inmemory";
-import gql from 'graphql-tag'
+import 'dotenv/config'
+console.log(process.env.MY_DATABASE_PASSWORD);
+import { execute, makePromise } from 'apollo-link';
+import fetch from 'node-fetch';
+import { HttpLink } from 'apollo-link-http';
+import gql from 'graphql-tag';
 
-const client = new ApolloClient({
-  link: new HttpLink({uri: 'https://localhost:3000'})
-  cache: new InMemoryCache()
+
+const uri = 'https://localhost:3000/pizza_api';
+const link = new HttpLink({ uri, fetch });
+
+const operation = {
+  query: gql`query { pizzas {
+    id
+  } }`,
+  variables: {}, //optional
+  operationName: {}, //optional
+  context: {}, //optional
+  extensions: {} //optional
+}
+
+// execute returns an Observable so it can be subscribed to
+execute(link, operation).subscribe({
+  next: data => console.log(`received data: ${JSON.stringify(data, null, 2)}`),
+  error: error => console.log(`received error ${error}`),
+  complete: () => console.log(`complete`),
 })
 
-client.query({
-    query: gql`
-    query AllUsers {
-      allUsers {
-        name
-        email
-      }
-    }
-  `,
-  })
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+// For single execution operations, a Promise can be used
+makePromise(execute(link, operation))
+  .then(data => console.log(`received data ${JSON.stringify(data, null, 2)}`))
+  .catch(error => console.log(`received error ${error}`))
